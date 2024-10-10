4ead6e0436dda56d907d629c8f8ad704ba93c37a
Sajithmym <sajithmym@gmail.com>
2024-10-10 22:10:52 +0530

    add testing

---
+++ Server.Tests/StudentLicenseServiceTests.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+// using Project.Service;
+
+namespace Project.Tests
+{
+    public class StudentLicenseServiceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Example test
+            Assert.True(true);
+        }
+
+    }
+}
